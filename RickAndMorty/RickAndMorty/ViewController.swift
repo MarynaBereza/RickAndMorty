097e765b4635f89e350b73ac1c9371afdb73b19a
MarynaBereza <bereza.maryna92@gmail.com>
2021-03-02 10:30:17 +0200

    Created the project. Added Navigation Controller without Navigation Bar.

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Maryna Bereza on 02.03.2021.
+//  Copyright © 2021 Bereza Maryna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
